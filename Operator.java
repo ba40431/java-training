11eae9f3e8b30c1646c7926c4b493c687aac5ad2
abby <ba40431@gmail.com>
2024-04-28 19:27:03 +0800

    add Operator file 2024-04-28

---
+++ Operator.java
@@ -0,0 +1,40 @@
+public class Operator {
+  public static void main(String[] args){
+    // 算術運算
+    // int x = 3 - 4;
+    // double y = 5.0 / 4.0;
+    // double z = 5 % 3.5;
+    // System.out.println(x);
+    // System.out.println(y);
+    // System.out.println(z);
+
+    // 指定運算
+    // int x = 5;
+    // x += 2;
+    // int y = 3;
+    // y *= 5;
+    // System.out.println(x);
+    // System.out.println(y);
+
+    // 比較運算
+    // boolean x = 3 > 5;
+    // boolean y = 3 == 3;
+    // boolean z = "Hello java" == "hello java";
+    // System.out.println(x);
+    // System.out.println(y);
+    // System.out.println(z);
+
+    // 單元運算
+    // int x = 4;
+    // x--;
+    // boolean y = !true;
+    // System.out.println(x);
+    // System.out.println(y);
+
+    // 邏輯運算
+    boolean x = false && true;
+    boolean y = false || true;
+    System.out.println(x);
+    System.out.println(y);
+  }
+}

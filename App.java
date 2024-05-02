bd0487f2d175a251dfd9a03ab19c37ae7f099d73
abby <ba40431@gmail.com>
2024-05-02 15:23:55 +0800

    use return 2024-05-02

--- App.java
+++ App.java
@@ -1,14 +1,28 @@
 class Test{
   public static void main(String[] args) {
     Test.hello("good");
-    Test.test2(2);
+    int addResult = BasicMath.add(5,4);
+    int multiplyResult = BasicMath.multiply(addResult, addResult);
+
+    System.out.println(addResult);
+    System.out.println(multiplyResult);
   }
 
   static void hello(String msg){
     System.out.println(msg);
   }
+}
 
-  static void test2(int x){
-    System.out.println(x);
+class BasicMath {
+  static int add(int x, int y){
+    int result = x + y;
+
+    return result;
+  }
+
+  static int multiply(int x, int y){
+    int result = x * y;
+
+    return result;
   }
 }

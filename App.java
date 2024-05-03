ae4148ac3a60c0fff597cb1f56e6524e9049f34c
abby <ba40431@gmail.com>
2024-05-03 19:07:50 +0800

    add math & gromerey folders 2024-05-03

--- App.java
+++ App.java
@@ -1,28 +1,13 @@
-class Test{
+import math.BasicMath;
+import math.geometry.Rectangle;
+
+class App {
   public static void main(String[] args) {
-    Test.hello("good");
-    int addResult = BasicMath.add(5,4);
-    int multiplyResult = BasicMath.multiply(addResult, addResult);
+    // 程式進入點
+    System.out.println(BasicMath.PI);
+    int add = BasicMath.add(5, 4);
+    System.out.println(add);
+    System.out.println(Rectangle.GOLDEN_RATIO);
 
-    System.out.println(addResult);
-    System.out.println(multiplyResult);
-  }
-
-  static void hello(String msg){
-    System.out.println(msg);
-  }
-}
-
-class BasicMath {
-  static int add(int x, int y){
-    int result = x + y;
-
-    return result;
-  }
-
-  static int multiply(int x, int y){
-    int result = x * y;
-
-    return result;
   }
 }
---
+++ math/BasicMath.java
@@ -0,0 +1,12 @@
+package math;
+
+public class BasicMath {
+  public static double PI = 3.1415926;
+  public static int add(int x, int y){
+    return x + y;
+  }
+
+  static int multiply(int x, int y){
+    return x * y;
+  }
+}

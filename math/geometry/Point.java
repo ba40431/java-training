0e4b87503ccad46f9242f5e42aad0889c3fe0d2d
abby <ba40431@gmail.com>
2024-05-03 21:42:32 +0800

    modify commit 2024-05-03

--- math/geometry/Point.java
+++ math/geometry/Point.java
@@ -2,7 +2,7 @@ package math.geometry;
 
 public class Point {
 
-  // 類別的屬性和方法
+  // 類別的屬性和方法(static)
   public static int ORIGIN_X = 0;
   public static int ORIGIN_Y = 0;
 
@@ -10,7 +10,7 @@ public class Point {
     System.out.println("ORIGIN_X: " + ORIGIN_X + "," + "ORIGIN_Y: " + ORIGIN_Y);
   }
 
-  // 物件的屬性、方法、建構式
+  // 物件的屬性、方法、建構式(non-static)
   public int x; 
   public int y;
   
